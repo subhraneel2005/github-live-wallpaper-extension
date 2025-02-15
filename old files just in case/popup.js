de001c54093b690b8f6c24a3050729634a5b47b3
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-15 23:04:01 +0530

    pnpm plasmo

--- old files just in case/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('setWallpaper').addEventListener('click', () => {
-    const imageUrl = document.getElementById('imageUrl').value;
-
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "setWallpaper", url: imageUrl });
-    });
-});
