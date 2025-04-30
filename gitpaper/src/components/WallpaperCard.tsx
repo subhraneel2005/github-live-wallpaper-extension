13d42683218f70f9bc5dbe4adac7ec81a66e3e93
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-05-01 05:28:23 +0530

    dashboard done + conditional set wallpaper button render

--- gitpaper/src/components/WallpaperCard.tsx
+++ gitpaper/src/components/WallpaperCard.tsx
@@ -1,32 +1,26 @@
 import { useEffect, useState } from "react"
 
 import type { WallpaperCardProps } from "~types/WallpaperProps"
-
 import { Button } from "./ui/button"
-import { Card, CardContent, CardFooter } from "./ui/card"
 
 export default function WallpaperCard({
   backgroundImageLink,
   bentoLink
 }: WallpaperCardProps) {
-  const [isExtensionContext, setIsExtensionContext] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
 
   return (
-    <Card>
-      <CardContent className="flex justify-center items-center">
-        <div
-          className="bg-cover bg-center flex justify-center items-center w-[80%] p-8 rounded-xl"
-          style={{ backgroundImage: `url(${backgroundImageLink})` }}>
-          <div className="h-full w-full overflow-hidden">
-            <img src={bentoLink} alt="Inner" className="w-full object-cover" />
-          </div>
+    <div className="relative w-full h-[200px] flex justify-center items-center flex-col rounded-xl bg-cover bg-center overflow-hidden"
+      style={{ backgroundImage: `url(${backgroundImageLink})` }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
+      <img src={bentoLink} alt="" className="w-2/3 object-cover" />
+
+      {isHovered && (
+        <div className="absolute top-0 left-0 z-10 w-full h-full flex justify-center items-center bg-black/50">
+          <Button>Set Wallpaper</Button>
         </div>
-      </CardContent>
-      <CardFooter className="flex justify-center items-center">
-        <Button disabled={!isExtensionContext}>
-          {isExtensionContext ? "Set wallpaper" : "Extension API not available"}
-        </Button>
-      </CardFooter>
-    </Card>
+      )}
+    </div>
   )
 }
--- gitpaper/src/popup.tsx
+++ gitpaper/src/popup.tsx
@@ -4,7 +4,7 @@ import Login from "./components/AuthComponents/Login"
 
 function IndexPopup() {
   return (
-    <div className="flex items-center justify-center h-[600px] w-[450px] flex-col">
+    <div className="flex items-center justify-center h-[500px] w-[700px] flex-col">
       <Login />
     </div>
   )
